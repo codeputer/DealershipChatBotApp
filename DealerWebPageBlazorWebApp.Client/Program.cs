691b3f5fd8d38a94c8232a6c02692c0748a1108d
richard@phoenixbussolutions.com <rihcard@phoenixbussolutions.com>
2024-11-27 20:53:18 -0800

    Why client side is clearing the values?

--- DealerWebPageBlazorWebApp.Client/Program.cs
+++ DealerWebPageBlazorWebApp.Client/Program.cs
@@ -1,18 +1,23 @@
 using DealerWebPageBlazorWebAppShared.DTOModels;
-
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using DealerWebPageBlazorWebAppClient; // Add this using directive
 
 //todo: review why this is the entry point never runs for the client side blazor applications!
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-var webAssembly = builder.Build();
+// Configure logging
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-IConfigurationRoot configuration = (IConfigurationRoot)webAssembly.Configuration;
+// Add a custom logging provider to send logs to the server
+builder.Logging.AddProvider(new ServerLoggingProvider(sp => sp.GetRequiredService<HttpClient>()));
+
+var webAssembly = builder.Build();
 
 await webAssembly.RunAsync();
